8cf8a302a60c2b0b976d12352cd84d36c05bc5ec
Timothy <oludefiyinfoluwa06@gmail.com>
2024-01-24 09:02:55 +0100

    Email sending logic has been added

--- client/src/components/Contact/ContactForm.js
+++ client/src/components/Contact/ContactForm.js
@@ -1,23 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const ContactForm = () => {
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+    const [message, setMessage] = useState();
+    const [error, setError] = useState();
+    const [msg, setMsg] = useState();
+
+    const handleClick = async (e) => {
+        e.preventDefault();
+
+        try {
+            const res = await axios.post('http://localhost:5000/', { name, email, message });
+            setMsg(res.data.message);
+            window.location.reload();
+        } catch (err) {
+            console.log(err);
+            if (err.response.data && err.response.data.error) {
+                setError(err.response.data.error);
+            }
+        }
+    }
+
+
     return (
         <form>
+            <p style={ msg ? { position: 'fixed', top: 0, left: 0, padding: 10, width: '100%', background: 'green', color: '#fff', textAlign: 'center' } : { display: 'none' }}>{msg}</p>
             <div className="form-group">
+                <p style={ error ? { textAlign: 'center', color: '#FF9999', width: '100%' } : { display: 'none' }}>{error}</p>
                 <div className="input-box">
                     <label htmlFor="name">Your name</label>
-                    <input type="text" id='name' name='name' />
+                    <input type="text" id='name' name='name' onChange={e => setName(e.target.value)} value={name} />
                 </div>
                 <div className="input-box">
                     <label htmlFor="email">Email</label>
-                    <input type="text" id='email' name='email' />
+                    <input type="text" id='email' name='email' onChange={e => setEmail(e.target.value)} value={email} />
                 </div>
             </div>
             <div className="input-box">
                 <label htmlFor="message">Message</label>
-                <textarea name="message" id="message"></textarea>
+                <textarea name="message" id="message" onChange={e => setMessage(e.target.value)} value={message}></textarea>
             </div>
-            <button>Submit</button>
+            <button onClick={handleClick}>Submit</button>
         </form>
     );
 }
---
+++ server/index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const validator = require('validator');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => res.send('Hello world!'));
+
+app.post('/', async (req, res) => {
+    const { name, email, message } = req.body;
+
+    if (!name || !email || !message) {
+        return res.status(400).json({ 'error': 'Input fields cannot be empty' });
+    }
+
+    if (!validator.isEmail(email)) {
+        return res.status(406).json({ 'error': 'Please enter a valid email' });
+    }
+
+    try {
+        const emailBody = `
+            <h1>${name}</h1>
+            <p>${email}</p>
+            <p>${message}</p>
+        `;
+
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASSWORD
+            }
+        });
+
+        const mailOptions = {
+            from: email,
+            to: 'oludefiyinfoluwa06@gmail.com',
+            subject: 'You just received an email from your website',
+            html: emailBody
+        }
+
+        await transporter.sendMail(mailOptions, (err, info) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).json({ 'error': 'An error occured' });
+            }
+
+            return res.status(200).json({ 'message': 'Email sent successfully' });
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ 'error': error.message });
+    }
+});
+
+app.listen(port, () => console.log('Server is up and running on port: http://localhost:' + port));
